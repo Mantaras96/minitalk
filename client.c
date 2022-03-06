e733b9a63b0029f16170878578e4618136cd58ff
Albert <albert.mantaras@gmail.com>
2022-03-06 19:17:35 +0100

    Norminette ok and test

--- client.c
+++ client.c
@@ -1,56 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amantara <amantara@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/06 17:39:52 by amantara          #+#    #+#             */
+/*   Updated: 2022/03/06 18:13:17 by amantara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-void show_error(int error)
-{
-    if (error == 1)
-    {
-        write(1, "Numero de parametro no es correcto", 35);
-    } else if (error == 2)
-    {
-        write(1, "El pid solo acepta caracteres numericos", 40);
-    }
+int	validar_pid(char *str)
+{	
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-void send_signal(int pid, char c)
+void	show_error(int error)
 {
-    int i;
-    
-    i = 8;
-	while (i--)
-    {
-		if (c >> i & 1)
-			kill(pid, SIGUSR2);
-		else
+	if (error == 1)
+	{
+		write(1, "Numero de parametro no es correcto", 35);
+	}
+	else if (error == 2)
+	{
+		write(1, "El pid solo acepta caracteres numericos", 40);
+	}
+}
+
+void	send_char(int pid, char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '1')
+		{
 			kill(pid, SIGUSR1);
+		}
+		else
+		{
+			kill(pid, SIGUSR2);
+		}
+		i++;
 		usleep(100);
 	}
 }
 
-void send_data(char *server_pid, char *str){
-    int i;
-    int pid;
-    (void)str;
-    pid = ft_atoi(server_pid);
-    i = 0;
-    while(str[i])
-    {
-        send_signal(pid, str[i]);
-        i++;
-    }
+// Transform array char to array binari codes 
+// (8 digits('0' or '1') for each letter)
+static char	*ft_to_bit(char *s, size_t i, size_t j)
+{
+	char	*ret;
+	int		c;
+	int		bytes;
 
+	i = ft_strlen(s);
+	ret = ft_calloc(i * 8 + 1, sizeof(char));
+	if (ret == NULL)
+		return (NULL);
+	while (i + 1 != 0)
+	{
+		c = s[i - 1];
+		bytes = 8;
+		while (bytes > 0)
+		{
+			if (c % 2 == 1)
+				ret[ft_strlen(s) * 8 - j - 1] = '1';
+			else
+				ret[ft_strlen(s) * 8 - j - 1] = '0';
+			c /= 2;
+			j++;
+			bytes--;
+		}
+		i--;
+	}
+	return (ret);
 }
 
-
-int main(int argc, char **argv){
-
-    if (argc == 3 && ft_strlen > 0){
-        send_data(argv[1], argv[2]);
-    } else {
-        show_error(1);
-    }
-    return(0);
+//Server pid + message will show server
+int	main(int argc, char **argv)
+{
+	if (argc == 3 && ft_strlen > 0 && validar_pid(argv[1]) == 1)
+		send_char(ft_atoi(argv[1]), ft_to_bit(argv[2], 0, 0));
+	else
+	{
+		if (validar_pid(argv[1]) == 1)
+			show_error (1);
+		else
+			show_error (2);
+	}
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,41 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amantara <amantara@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/06 17:38:06 by amantara          #+#    #+#             */
+/*   Updated: 2022/03/06 17:39:04 by amantara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
+char	*ft_append_char( char *str, char c)
+{	
+	char	*str2;
+	int		i;
 
-void		print_pid(void)
-{
-    char *tmp_pid;
-
-    tmp_pid = ft_itoa(getpid());
-    write(1, "pid: ", 5);
-    write(1, tmp_pid, ft_strlen(tmp_pid));
-    write(1, "\n", 1);
-    free(tmp_pid);
+	i = 0;
+	str2 = malloc(sizeof(char) * (ft_strlen(str) + 2));
+	if (!str2)
+		return (NULL);
+	while (str[i])
+	{
+		str2[i] = str[i];
+		i++;
+	}
+	str2[i] = c;
+	str2[i + 1] = '\0';
+	free(str);
+	return (str2);
 }
 
-void recibir (int sig, siginfo_t *info, void *context){
-    (void)sig;
-    (void)info;
-    (void)context;
-    if (sig == SIGUSR1)
-        write(1, "1", 1);
-    else if (sig == SIGUSR2)
-        write(1, "0", 1);
+void	print_pid(void)
+{
+	char	*tmp_pid;
+
+	tmp_pid = ft_itoa(getpid());
+	write(1, "pid: ", 5);
+	write(1, tmp_pid, ft_strlen(tmp_pid));
+	write(1, "\n", 1);
+	free(tmp_pid);
 }
 
-int main(void)
+void	binary_to_decimal(char *str)
 {
-    struct sigaction	s_sigaction;
+	int				acumulado;
+	unsigned char	c;
+	size_t			i;
 
-    print_pid();
-    s_sigaction.sa_sigaction = recibir;
-    s_sigaction.sa_flags = SA_SIGINFO;
-    sigaction(SIGUSR1, &s_sigaction, 0);
-	sigaction(SIGUSR2, &s_sigaction, 0);
-    while (1)
+	c = 0;
+	i = 0;
+	acumulado = 128;
+	while (str[i])
+	{
+		c += acumulado * (str[i] - '0');
+		acumulado = acumulado / 2;
+		i++;
+	}
+	write(1, &c, 1);
+}
+
+// void recibir (int sig){
+// 	static char	*bits;
+// 	static int	bitcount;
+
+// 	bitcount++;
+// 	if(!bits)
+// 	{
+// 		bits = ft_strdup("");
+// 		bitcount = 1;
+// 	}
+// 	if (sig == SIGUSR1)
+// 		ft_strjoinC(bits, '1');
+// 	else if (sig == SIGUSR2)
+// 		ft_strjoinC(bits, '0');
+// 	if (bitcount == 8)
+// 	{
+
+// 		binary_to_decimal(bits);
+// 		free(bits);
+// 		bits = NULL;
+// 	}
+// }
+
+// reads every bit and appends it to bits
+// if its a 8 bit it prints the char to the console
+static void	ft_confirm(int sig)
+{
+	static char	*bits;
+	static int	bitcount;
+	int			i;
+
+	bitcount++;
+	if (bits == NULL)
+	{
+		bits = ft_strdup("");
+		bitcount = 1;
+	}
+	if (sig == SIGUSR2)
+		bits = ft_append_char(bits, '0');
+	else
+		bits = ft_append_char(bits, '1');
+	if (bitcount == 8)
+	{
+		i = 0;
+		binary_to_decimal(bits);
+		free(bits);
+		bits = NULL;
+	}
+}
+
+int	main(void)
+{
+	print_pid();
+	while (1)
+	{
+		signal(SIGUSR1, ft_confirm);
+		signal(SIGUSR2, ft_confirm);
 		pause();
-    return(0);
+	}
+	return (0);
 }
